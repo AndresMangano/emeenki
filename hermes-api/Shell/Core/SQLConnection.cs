4e41c374c1c23a80d7482e20ada53779e65b5db1
Leonardo Mangano <manganosuarezl@gmail.com>
2021-02-09 19:06:55 -0300

    Some changes

--- hermes-api/Shell/Core/SQLConnection.cs
+++ hermes-api/Shell/Core/SQLConnection.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Threading;
 using DbUp;
 
 namespace Hermes.Shell
@@ -11,14 +13,23 @@ namespace Hermes.Shell
         {
             ConnectionString = connectionString;
 
-            var upgraderResult =
-                DeployChanges
-                    .To
-                    .MySqlDatabase(connectionString)
-                    .WithScriptsAndCodeEmbeddedInAssembly(Assembly.GetExecutingAssembly())
-                    .LogToConsole()
-                    .Build()
-                    .PerformUpgrade();
+            for (var retries=10; retries > 0; ) {
+                try {
+                    var upgraderResult =
+                        DeployChanges
+                            .To
+                            .MySqlDatabase(connectionString)
+                            .WithScriptsAndCodeEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                            .LogToConsole()
+                            .Build()
+                            .PerformUpgrade();
+                    break;
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                    retries--;
+                    Thread.Sleep(5000);
+                }
+            }
         }
 
         public void DestroyDB()

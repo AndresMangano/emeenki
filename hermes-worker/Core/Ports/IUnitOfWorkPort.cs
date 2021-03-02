df33aed282368a5bfb592f9cdc4ea0899812fdc8
Leonardo Mangano <manganosuarezl@gmail.com>
2021-03-02 17:07:04 -0300

    Clean up

--- hermes-worker/Core/Ports/IUnitOfWorkPort.cs
+++ hermes-worker/Core/Ports/IUnitOfWorkPort.cs
@@ -3,9 +3,5 @@ using Hermes.Worker.Core.Model;
 
 namespace Hermes.Worker.Core.Ports
 {
-    public interface IUnitOfWorkPort<IO>
-    {
-        void Transaction<K, V>(DefaultMessage<K, V> message, Action<IO> unitOfWork);
-    }
     public interface DBInterpreter<IO> {}
 }

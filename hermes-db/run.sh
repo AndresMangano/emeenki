15122ed53bbb8bae9f523a361dcb4a259081047f
Leonardo Mangano <manganosuarezl@gmail.com>
2021-03-21 18:28:10 -0300

    Setup local environment

--- hermes-db/run.sh
+++ hermes-db/run.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
-for arg in $@
-do
-    case $arg in
-        start)
-            docker service create \
-                --name hermes-db \
-                --network hermes-network \
-                --publish 3306:3306 \
-                --mount type=volume,src=db_data,dst=/var/lib/mysql \
-                --env MYSQL_DATABASE=hermes \
-                --env MYSQL_ROOT_PASSWORD=${HERMES_DB_ROOT_PASSWORD:-"f90ef86b-b326-4188-a08d-af18fc547aa0"} \
-                mysql:5.7.29
-            ;;
-        update)
-            docker service update hermes-db --force
-            ;;
-        stop)
-            docker service rm hermes-db
-            ;;
-        *)
-            echo "Invalid command"
-            ;;
-    esac
-done
+docker service create \
+    --name hermes-db \
+    --publish 3306:3306 \
+    --mount type=volume,src=db_data,dst=/var/lib/mysql \
+    --env MYSQL_DATABASE=hermes \
+    --env MYSQL_ROOT_PASSWORD=${HERMES_DB_ROOT_PASSWORD:-"f90ef86b-b326-4188-a08d-af18fc547aa0"} \
+    mysql:5.7.29
